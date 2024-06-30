178f3772c2caaf1b2fb53974a7e01310d78c2b17
Alexis Halverson <alexis.halverson@seermedical.com>
2024-06-30 22:24:08 +1000

    add chip packet

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -4,11 +4,14 @@ const PARCHMENT = "#F3E9D2";
 const PEACH = "#F7E3AF";
 const CARROT_ORANGE = "#F18F01";
 const CHESTNUT = "#C73E1D";
+const AUTUMN_BROWN = "#541a0b";
 const AMARANTH_PINK = "#E39EC1";
 const FRENCH_MAUVE = "#C47AC0";
 const CAROLINA_BLUE = "#84BCDA";
+const PISTACHIO = "#B4DC7F"
 const SPACE_CADET = "#2B2D42";
-const BACKGROUND = SPACE_CADET
+const LAVENDER_GREY = "#a3a6c9"
+const BACKGROUND = SPACE_CADET;
 
 // gravity
 const g = 9.8;
@@ -37,10 +40,10 @@ function setup() {
 
   cX = windowWidth / 2;
   cY = windowHeight / 2;
-  refreshData();
-  dataTimer = setInterval(() => {
-    refreshData();
-  }, 1000 * 60);
+  // refreshData();
+  // dataTimer = setInterval(() => {
+  //   refreshData();
+  // }, 1000 * 60);
 
   //   cV = 0.182;
 }
@@ -68,8 +71,6 @@ function draw() {
   //   console.log("cY", cY);
   background(BACKGROUND);
 
-  fill("orange");
-
   //   const data = getItem(binDataKey) as (null | BinSensorDataType)
 
   //   if (data) {
@@ -82,7 +83,23 @@ function draw() {
   //     }
   //   }
 
-  drawBottle(windowWidth / 2, windowHeight / 2, 100, frameCount * 0.01);
+  
+
+  drawBottle(windowWidth / 2 - 100, windowHeight / 2, 100, frameCount * 0.01);
+
+  drawAppleCore(
+    windowWidth / 2 + 100,
+    windowHeight / 2,
+    100,
+    10 + frameCount * 0.01
+  );
+
+  drawChipPacket(
+    windowWidth / 2,
+    windowHeight / 2 - 200,
+    100,
+    20 + frameCount * 0.01
+  );
 }
 
 /**
@@ -94,17 +111,16 @@ function draw() {
  * @param r (optional) rotation of the bottle.
  */
 function drawBottle(x: number, y: number, s: number, r: number = 0) {
-  const w = s / 2;
+  const w = s * 0.42;
   const h = s;
   push();
   translate(x, y);
-  rotate(r)
+  rotate(r);
   noStroke();
   // plastic body
   fill(CAROLINA_BLUE);
   const rX = 0;
   const rW = w;
-  const rH = h - w / 2;
 
   const cX = 0;
   const cD = w;
@@ -113,12 +129,13 @@ function drawBottle(x: number, y: number, s: number, r: number = 0) {
   const eW = w;
   const eH = w / 2;
 
-  const rY = h - (cD / 2 + eH / 2);
+  const rY =  0;
+  const rH = h - (cD / 2 + eH / 2);
+
 
   const cY = rY - rH / 2;
 
   const eY = rY + rH / 2;
-
 
   circle(cX, cY, cD);
   rect(rX, rY, rW, rH);
@@ -131,19 +148,17 @@ function drawBottle(x: number, y: number, s: number, r: number = 0) {
   rect(0, cY - cD / 2, capW, capH, w / 10);
 
   // label
-    fill(CHESTNUT)
-    const lH = rH/2
-    const lrX = rY - lH / 3
-    rect(0, lrX, rW, lH)
+  fill(FRENCH_MAUVE);
+  const lH = rH / 2;
+  const lrX = rY - lH / 3;
+  rect(0, lrX, rW, lH);
 
-    ellipse(0, lrX + lH / 2, eW, eH);
-    fill(CAROLINA_BLUE)
-    ellipse(0, lrX - lH / 2, eW, eH);
-
+  ellipse(0, lrX + lH / 2, eW, eH);
+  fill(CAROLINA_BLUE);
+  ellipse(0, lrX - lH / 2, eW, eH);
 
   pop();
 }
-
 
 /**
  * Draws an apple core to the canvas.
@@ -154,16 +169,99 @@ function drawBottle(x: number, y: number, s: number, r: number = 0) {
  * @param r (optional) rotation of the apple core.
  */
 function drawAppleCore(x: number, y: number, s: number, r: number = 0) {
-    const w = s / 2;
-    const h = s;
-    push();
-    translate(x, y);
-    rotate(frameCount * 0.01)
-    noStroke();
-    
+  const h = s * 0.6;
+
+  const w = h / 2;
+  push();
+  translate(x, y);
+  rotate(r);
+  noStroke();
+
+  const seH = w / 3;
+  const rH = h - seH / 2;
+
+  // bottom skin
+
+  fill(PISTACHIO);
+  rect(0, rH / 2, w, seH, s / 10);
+
+  // apple flesh
+  fill(PEACH);
+  rect(0, 0, w, rH);
+  fill(BACKGROUND);
+  ellipse(-(w / 2), 0, w / 2, h);
+  ellipse(w / 2, 0, w / 2, h);
+
+  // top skin
+  fill(PISTACHIO);
+  ellipse(0, -rH / 2, w, seH);
+
+  // stalk
+  fill(AUTUMN_BROWN);
+  const sH = s / 5;
+  push();
+  translate(0, -rH / 2 - sH / 2);
+  rotate(0.2);
+  rect(0, 0, sH / 3, sH);
+  pop();
+
+  // pip
+  push();
+  rotate(-0.2);
+  ellipse(w / 15, -w / 12, w / 6, w / 4);
+  pop();
+  pop();
+}
+
+/**
+ * Draws an apple core to the canvas.
+ *
+ * @param x x-coordinate of the center of the apple core.
+ * @param y y-coordinate of the center of the apple core.
+ * @param s height of the apple core.
+ * @param r (optional) rotation of the apple core.
+ */
+function drawChipPacket(x: number, y: number, s: number, r: number = 0) {
+  const h = s * 0.8;
+
+  const w = h * 0.8;
+  push();
+  translate(x, y);
+  rotate(r);
+  noStroke();
+
+  // packet
+  fill(AMARANTH_PINK);
+
+  const feH = s / 5;
+
+  const rH = h - feH / 2;
+  rect(0, feH / 2, w, rH);
+
+  // top foil
+  // fill(SPACE_CADET)
+
+  // const gap = feH / 8
+  // rect(0, - (rH /2 ) + feH / 2 + gap, w + gap * 2, feH, feH / 2)
+
+  fill(LAVENDER_GREY)
   
-    pop();
-  }
+  rect(0, - (rH /2 ) + feH / 2, w, feH, feH / 2 )
+
+  // bottom foil
+
+  const bfrH = feH / 3
+
+  rect(0, (rH /2 ) + feH / 2 - bfrH /2, w, bfrH)
+
+  // label
+
+  fill(FRENCH_MAUVE)
+
+  rect(0, 0 + feH / 2, w / 2, w /3, s /10)
+
+  pop();
+}
 
 function refreshData() {
   getBinSensorData()
