f7e475dda1eb898671ae8560b95dd1ccb53c26de
Alexis Halverson <alexis.halverson@seermedical.com>
2024-07-01 22:53:31 +1000

    add bin data functions

--- sketch/data.ts
+++ sketch/data.ts
@@ -7,25 +7,26 @@ function melbDataUrl(datasetId: string): URL {
   return new URL(`${melbOpenDataBaseUrl}${datasetId}/records`);
 }
 
-interface BinSensorDataType {
+interface BinSensorDataEntry {
+  dev_id: string;
+  time: string;
+  temperature: number;
+  distance: number;
+  filllevel: number;
+  battery: number;
+  lat_long: {
+    lon: number;
+    lat: number;
+  };
+  sensor_name: string;
+  fill_level: number;
+}
+interface BinSensorDataReturnType {
   total_count: number;
-  results: {
-    dev_id: string;
-    time: string;
-    temperature: number;
-    distance: number;
-    filllevel: number;
-    battery: number;
-    lat_long: {
-      lon: number;
-      lat: number;
-    };
-    sensor_name: string;
-    fill_level: number;
-  }[];
+  results: BinSensorDataEntry[];
 }
 
-function isBinSensorData(data: any): data is BinSensorDataType {
+function isBinSensorData(data: any): data is BinSensorDataReturnType {
   if (data instanceof Object && "total_count" in data && "results" in data) {
     if (data.results instanceof Array) {
       if (data.results.length > 0) {
@@ -39,10 +40,37 @@ function isBinSensorData(data: any): data is BinSensorDataType {
   return false;
 }
 
-async function getBinSensorData(): Promise<BinSensorDataType> {
+interface BinSensorDevIdReturnType {
+  results: {
+    dev_id: string;
+  }[];
+}
+
+function isBinSensorDevIds(data: any): data is BinSensorDevIdReturnType {
+  if ("results" in data) {
+    if (data.results.length > 0) {
+      if ("dev_id" in data.results[0]) {
+        return true;
+      }
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function getBinSensorData(
+  id?: string,
+  after_date?: Date
+): Promise<BinSensorDataReturnType["results"]> {
   const url = melbDataUrl(binSensorDatasetId);
 
-  url.search = new URLSearchParams({ limit: "100" }).toString();
+  url.search = new URLSearchParams({
+    where: `${id ? `dev_id = "${id}"` : ""}${id && after_date ? " and " : ""}${
+      after_date ? `time >= date'${after_date.toISOString()}'` : ""
+    }`,
+    limit: "100",
+  }).toString();
 
   const response = await fetch(url);
 
@@ -55,7 +83,7 @@ async function getBinSensorData(): Promise<BinSensorDataType> {
     if (!isBinSensorData(json)) {
       throw new Error(`Unexpected response format: ${json}`);
     }
-    return json;
+    return json.results;
   } catch (e) {
     if (e instanceof Error) {
       console.log(`Request for ${url} failed:`, e.message);
@@ -63,4 +91,49 @@ async function getBinSensorData(): Promise<BinSensorDataType> {
 
     console.log(`Request for ${url} failed with weird error:`, e);
   }
+}
+
+async function getBinSensorIds(): Promise<string[]> {
+  const url = melbDataUrl(binSensorDatasetId);
+
+  url.search = new URLSearchParams({
+    group_by: "dev_id",
+    limit: "100",
+  }).toString();
+
+  const response = await fetch(url);
+
+  try {
+    // if (!response.ok) {
+    //   throw new Error(`Response status: ${response.status}`);
+    // }
+
+    const json = await response.json();
+    if (!isBinSensorDevIds(json)) {
+      throw new Error(`Unexpected response format: ${json}`);
+    }
+
+    return json.results.map((o) => o.dev_id);
+  } catch (e) {
+    if (e instanceof Error) {
+      console.log(`Request for ${url} failed:`, e.message);
+    }
+
+    console.log(`Request for ${url} failed with weird error:`, e);
+  }
+}
+
+async function getBinSensorDataForDate(date: Date): Promise<
+  {
+    id: string;
+    data: BinSensorDataEntry[];
+  }[]
+> {
+  const sensorIds = await getBinSensorIds();
+
+  const binData = await Promise.all(
+    sensorIds.map((id) => getBinSensorData(id, date))
+  );
+
+  return sensorIds.map((id, i) => ({ id, data: binData[i] }));
 }
